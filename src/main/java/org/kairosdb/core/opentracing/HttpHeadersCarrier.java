7c811c2bfe98d95f00ee4251a11fe9a5eea5b999
Vibhor Goyal <vibhor.goyal@zalando.de>
2018-03-09 19:31:15 +0100

    opentracing instrumentation

---
+++ src/main/java/org/kairosdb/core/opentracing/HttpHeadersCarrier.java
@@ -0,0 +1,33 @@
+package org.kairosdb.core.opentracing;
+
+import io.opentracing.contrib.web.servlet.filter.HttpServletRequestExtractAdapter;
+import io.opentracing.propagation.TextMap;
+import javax.ws.rs.core.MultivaluedMap;
+
+import java.util.Iterator;
+import java.util.Map;
+
+public class HttpHeadersCarrier implements TextMap{
+
+    public HttpHeadersCarrier()  {
+        this.httpHeaders = null;
+    }
+
+    private final MultivaluedMap<String, String> httpHeaders;
+
+    public HttpHeadersCarrier(MultivaluedMap<String, String> httpHeaders)  {
+        this.httpHeaders = httpHeaders;
+    }
+
+    @Override
+    public void put(String key, String value) {
+        throw new UnsupportedOperationException(
+                HttpHeadersCarrier.class.getName() +" should only be used with Tracer.extract()");
+    }
+
+    @Override
+    public Iterator<Map.Entry<String, String>> iterator() {
+        return new HttpServletRequestExtractAdapter.MultivaluedMapFlatIterator<>(httpHeaders.entrySet());
+    }
+
+}
