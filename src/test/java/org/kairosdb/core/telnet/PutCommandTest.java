beb77e5acd1666faff95dc147c9525c07012c1d2
Alex Korotkykh <me@alexkorotkikh.com>
2019-02-15 14:17:00 +0100

    upgrade netty to the latest 3.x

--- src/test/java/org/kairosdb/core/telnet/PutCommandTest.java
+++ src/test/java/org/kairosdb/core/telnet/PutCommandTest.java
@@ -47,331 +47,270 @@ import org.kairosdb.util.ValidationException;
 
 import com.google.common.collect.ImmutableSortedMap;
 
-public class PutCommandTest
-{
-	private PutCommand command;
-	private FakeDatastore datastore;
-	private static MockTracer tracer = new MockTracer();
+public class PutCommandTest {
+    private PutCommand command;
+    private FakeDatastore datastore;
+    private static MockTracer tracer = new MockTracer();
 
-	@Before
-	public void setup() throws DatastoreException
-	{
-		datastore = new FakeDatastore();
-		KairosDatastore kairosDatastore = new KairosDatastore(datastore, new QueryQueuingManager(1, "test"),
-				Collections.<DataPointListener>emptyList(), new TestDataPointFactory(), tracer);
-		command = new PutCommand(kairosDatastore, "test", new LongDataPointFactoryImpl(),
-				new DoubleDataPointFactoryImpl());
-	}
+    @Before
+    public void setup() throws DatastoreException {
+        datastore = new FakeDatastore();
+        KairosDatastore kairosDatastore = new KairosDatastore(datastore, new QueryQueuingManager(1, "test"),
+                Collections.<DataPointListener>emptyList(), new TestDataPointFactory(), tracer);
+        command = new PutCommand(kairosDatastore, "test", new LongDataPointFactoryImpl(),
+                new DoubleDataPointFactoryImpl());
+    }
 
-	@Test
-	public void test() throws DatastoreException, ValidationException
-	{
-		command.execute(new FakeChannel(), new String[]{"telnet", "MetricName", "12345678999", "789", "foo=bar", "fum=barfum"});
+    @Test
+    public void test() throws DatastoreException, ValidationException {
+        command.execute(new FakeChannel(), new String[]{"telnet", "MetricName", "12345678999", "789", "foo=bar", "fum=barfum"});
 
-		assertThat(datastore.getSet().getName(), equalTo("MetricName"));
-		assertThat(datastore.getSet().getTags().size(), equalTo(2));
-		assertThat(datastore.getSet().getTags().get("foo"), equalTo("bar"));
-		assertThat(datastore.getSet().getTags().get("fum"), equalTo("barfum"));
-		assertThat(datastore.getSet().getDataPoints().get(0).getTimestamp(), equalTo(12345678999L));
-		assertThat(datastore.getSet().getDataPoints().get(0).getLongValue(), equalTo(789L));
-	}
+        assertThat(datastore.getSet().getName(), equalTo("MetricName"));
+        assertThat(datastore.getSet().getTags().size(), equalTo(2));
+        assertThat(datastore.getSet().getTags().get("foo"), equalTo("bar"));
+        assertThat(datastore.getSet().getTags().get("fum"), equalTo("barfum"));
+        assertThat(datastore.getSet().getDataPoints().get(0).getTimestamp(), equalTo(12345678999L));
+        assertThat(datastore.getSet().getDataPoints().get(0).getLongValue(), equalTo(789L));
+    }
 
-	@Test
-	public void test_metricName_empty_invalid() throws DatastoreException, ValidationException
-	{
-		try
-		{
-			command.execute(new FakeChannel(), new String[]{"telnet", "", "12345678999", "789", "foo=bar", "fum=barfum"});
-			fail("ValidationException expected");
-		}
-		catch (DatastoreException e)
-		{
-			fail("ValidationException expected");
-		}
-		catch (ValidationException e)
-		{
-			assertThat(e.getMessage(), equalTo("metricName may not be empty."));
-		}
-	}
+    @Test
+    public void test_metricName_empty_invalid() throws DatastoreException, ValidationException {
+        try {
+            command.execute(new FakeChannel(), new String[]{"telnet", "", "12345678999", "789", "foo=bar", "fum=barfum"});
+            fail("ValidationException expected");
+        } catch (DatastoreException e) {
+            fail("ValidationException expected");
+        } catch (ValidationException e) {
+            assertThat(e.getMessage(), equalTo("metricName may not be empty."));
+        }
+    }
 
-	@Test
-	public void test_metricName_characters_valid() throws DatastoreException, ValidationException
-	{
-		command.execute(new FakeChannel(), new String[]{"telnet", "你好", "12345678999", "789", "foo=bar", "fum=barfum"});
-	}
+    @Test
+    public void test_metricName_characters_valid() throws DatastoreException, ValidationException {
+        command.execute(new FakeChannel(), new String[]{"telnet", "你好", "12345678999", "789", "foo=bar", "fum=barfum"});
+    }
 
-	@Test
-	public void test_tagName_empty_invalid() throws DatastoreException, ValidationException
-	{
-		try
-		{
-			command.execute(new FakeChannel(), new String[]{"telnet", "metricName", "12345678999", "789", "foo=bar", "=barfum"});
-			fail("ValidationException expected");
-		}
-		catch (DatastoreException e)
-		{
-			fail("ValidationException expected");
-		}
-		catch (ValidationException e)
-		{
-			assertThat(e.getMessage(), equalTo("tag[1].name may not be empty."));
-		}
-	}
+    @Test
+    public void test_tagName_empty_invalid() throws DatastoreException, ValidationException {
+        try {
+            command.execute(new FakeChannel(), new String[]{"telnet", "metricName", "12345678999", "789", "foo=bar", "=barfum"});
+            fail("ValidationException expected");
+        } catch (DatastoreException e) {
+            fail("ValidationException expected");
+        } catch (ValidationException e) {
+            assertThat(e.getMessage(), equalTo("tag[1].name may not be empty."));
+        }
+    }
 
-	@Test
-	public void test_tagName_characters_invalid() throws DatastoreException, ValidationException
-	{
-		try
-		{
-			command.execute(new FakeChannel(), new String[]{"telnet", "metricName", "12345678999", "789", "foo=bar", "fum:fi=barfum"});
-			fail("ValidationException expected");
-		}
-		catch (DatastoreException e)
-		{
-			fail("ValidationException expected");
-		}
-		catch (ValidationException e)
-		{
-			assertThat(e.getMessage(), equalTo("tag[1].name may contain any character except colon ':', and equals '='."));
-		}
-	}
+    @Test
+    public void test_tagName_characters_invalid() throws DatastoreException, ValidationException {
+        try {
+            command.execute(new FakeChannel(), new String[]{"telnet", "metricName", "12345678999", "789", "foo=bar", "fum:fi=barfum"});
+            fail("ValidationException expected");
+        } catch (DatastoreException e) {
+            fail("ValidationException expected");
+        } catch (ValidationException e) {
+            assertThat(e.getMessage(), equalTo("tag[1].name may contain any character except colon ':', and equals '='."));
+        }
+    }
 
-	@Test
-	public void test_tagValue_empty_invalid() throws DatastoreException, ValidationException
-	{
-		try
-		{
-			command.execute(new FakeChannel(), new String[]{"telnet", "metricName", "12345678999", "789", "foo=bar", "fum="});
-			fail("ValidationException expected");
-		}
-		catch (DatastoreException e)
-		{
-			fail("ValidationException expected");
-		}
-		catch (ValidationException e)
-		{
-			assertThat(e.getMessage(), equalTo("tag[1] must be in the format 'name=value'."));
-		}
-	}
+    @Test
+    public void test_tagValue_empty_invalid() throws DatastoreException, ValidationException {
+        try {
+            command.execute(new FakeChannel(), new String[]{"telnet", "metricName", "12345678999", "789", "foo=bar", "fum="});
+            fail("ValidationException expected");
+        } catch (DatastoreException e) {
+            fail("ValidationException expected");
+        } catch (ValidationException e) {
+            assertThat(e.getMessage(), equalTo("tag[1] must be in the format 'name=value'."));
+        }
+    }
 
-	@Test
-	public void test_tagValue_characters_invalid() throws DatastoreException, ValidationException
-	{
-		try
-		{
-			command.execute(new FakeChannel(), new String[]{"telnet", "metricName", "12345678999", "789", "foo=bar", "fum=bar:fum"});
-			fail("ValidationException expected");
-		}
-		catch (DatastoreException e)
-		{
-			fail("ValidationException expected");
-		}
-		catch (ValidationException e)
-		{
-			assertThat(e.getMessage(), equalTo("tag[1].value may contain any character except colon ':', and equals '='."));
-		}
-	}
+    @Test
+    public void test_tagValue_characters_invalid() throws DatastoreException, ValidationException {
+        try {
+            command.execute(new FakeChannel(), new String[]{"telnet", "metricName", "12345678999", "789", "foo=bar", "fum=bar:fum"});
+            fail("ValidationException expected");
+        } catch (DatastoreException e) {
+            fail("ValidationException expected");
+        } catch (ValidationException e) {
+            assertThat(e.getMessage(), equalTo("tag[1].value may contain any character except colon ':', and equals '='."));
+        }
+    }
 
-	@Test
-	public void test_tag_invalid() throws DatastoreException, ValidationException
-	{
-		try
-		{
-			command.execute(new FakeChannel(), new String[]{"telnet", "metricName", "12345678999", "789", "foo=bar", "fum-barfum"});
-			fail("ValidationException expected");
-		}
-		catch (DatastoreException e)
-		{
-			fail("ValidationException expected");
-		}
-		catch (ValidationException e)
-		{
-			assertThat(e.getMessage(), equalTo("tag[1] must be in the format 'name=value'."));
-		}
-	}
+    @Test
+    public void test_tag_invalid() throws DatastoreException, ValidationException {
+        try {
+            command.execute(new FakeChannel(), new String[]{"telnet", "metricName", "12345678999", "789", "foo=bar", "fum-barfum"});
+            fail("ValidationException expected");
+        } catch (DatastoreException e) {
+            fail("ValidationException expected");
+        } catch (ValidationException e) {
+            assertThat(e.getMessage(), equalTo("tag[1] must be in the format 'name=value'."));
+        }
+    }
 
-	public class FakeChannel implements Channel
-	{
-		@Override
-		public Integer getId()
-		{
-			return null;
-		}
+    public class FakeChannel implements Channel {
+        @Override
+        public Integer getId() {
+            return null;
+        }
 
-		@Override
-		public ChannelFactory getFactory()
-		{
-			return null;
-		}
+        @Override
+        public ChannelFactory getFactory() {
+            return null;
+        }
 
-		@Override
-		public Channel getParent()
-		{
-			return null;
-		}
+        @Override
+        public Channel getParent() {
+            return null;
+        }
 
-		@Override
-		public ChannelConfig getConfig()
-		{
-			return null;
-		}
+        @Override
+        public ChannelConfig getConfig() {
+            return null;
+        }
 
-		@Override
-		public ChannelPipeline getPipeline()
-		{
-			return null;
-		}
+        @Override
+        public ChannelPipeline getPipeline() {
+            return null;
+        }
 
-		@Override
-		public boolean isOpen()
-		{
-			return false;
-		}
+        @Override
+        public boolean isOpen() {
+            return false;
+        }
 
-		@Override
-		public boolean isBound()
-		{
-			return false;
-		}
+        @Override
+        public boolean isBound() {
+            return false;
+        }
 
-		@Override
-		public boolean isConnected()
-		{
-			return false;
-		}
+        @Override
+        public boolean isConnected() {
+            return false;
+        }
 
-		@Override
-		public SocketAddress getLocalAddress()
-		{
-			return null;
-		}
+        @Override
+        public SocketAddress getLocalAddress() {
+            return null;
+        }
 
-		@Override
-		public SocketAddress getRemoteAddress()
-		{
-			return null;
-		}
+        @Override
+        public SocketAddress getRemoteAddress() {
+            return null;
+        }
 
-		@Override
-		public ChannelFuture write(Object o)
-		{
-			return null;
-		}
+        @Override
+        public ChannelFuture write(Object o) {
+            return null;
+        }
 
-		@Override
-		public ChannelFuture write(Object o, SocketAddress socketAddress)
-		{
-			return null;
-		}
+        @Override
+        public ChannelFuture write(Object o, SocketAddress socketAddress) {
+            return null;
+        }
 
-		@Override
-		public ChannelFuture bind(SocketAddress socketAddress)
-		{
-			return null;
-		}
+        @Override
+        public ChannelFuture bind(SocketAddress socketAddress) {
+            return null;
+        }
 
-		@Override
-		public ChannelFuture connect(SocketAddress socketAddress)
-		{
-			return null;
-		}
+        @Override
+        public ChannelFuture connect(SocketAddress socketAddress) {
+            return null;
+        }
 
-		@Override
-		public ChannelFuture disconnect()
-		{
-			return null;
-		}
+        @Override
+        public ChannelFuture disconnect() {
+            return null;
+        }
 
-		@Override
-		public ChannelFuture unbind()
-		{
-			return null;
-		}
+        @Override
+        public ChannelFuture unbind() {
+            return null;
+        }
 
-		@Override
-		public ChannelFuture close()
-		{
-			return null;
-		}
+        @Override
+        public ChannelFuture close() {
+            return null;
+        }
 
-		@Override
-		public ChannelFuture getCloseFuture()
-		{
-			return null;
-		}
+        @Override
+        public ChannelFuture getCloseFuture() {
+            return null;
+        }
 
-		@Override
-		public int getInterestOps()
-		{
-			return 0;
-		}
+        @Override
+        public int getInterestOps() {
+            return 0;
+        }
 
-		@Override
-		public boolean isReadable()
-		{
-			return false;
-		}
+        @Override
+        public boolean isReadable() {
+            return false;
+        }
 
-		@Override
-		public boolean isWritable()
-		{
-			return false;
-		}
+        @Override
+        public boolean isWritable() {
+            return false;
+        }
 
-		@Override
-		public ChannelFuture setInterestOps(int i)
-		{
-			return null;
-		}
+        @Override
+        public ChannelFuture setInterestOps(int i) {
+            return null;
+        }
 
-		@Override
-		public ChannelFuture setReadable(boolean b)
-		{
-			return null;
-		}
+        @Override
+        public ChannelFuture setReadable(boolean b) {
+            return null;
+        }
 
-        // @Override
-        // public Object getAttachment()
-        // {
-        // return null;
-        // }
-        //
-        // @Override
-        // public void setAttachment(Object o)
-        // {
-        // }
+        @Override
+        public boolean getUserDefinedWritability(int i) {
+            return false;
+        }
 
-		@Override
-		public int compareTo(Channel o)
-		{
-			return 0;
-		}
-	}
+        @Override
+        public void setUserDefinedWritability(int i, boolean b) {
 
-	private class FakeDatastore implements Datastore
-	{
-		private DataPointSet set;
+        }
 
-		private DataPointSet getSet()
-		{
-			return set;
-		}
+        @Override
+        public Object getAttachment() {
+            return null;
+        }
 
-		@Override
-		public void close() throws InterruptedException, DatastoreException
-		{
-		}
+        @Override
+        public void setAttachment(Object o) {
+        }
 
-		@Override
-		public void putDataPoint(String metricName,
-				ImmutableSortedMap<String, String> tags,
-				DataPoint dataPoint, int ttl) throws DatastoreException
-		{
-			if (set == null)
-				set = new DataPointSet(metricName, tags, Collections.EMPTY_LIST);
+        @Override
+        public int compareTo(Channel o) {
+            return 0;
+        }
+    }
 
-			set.addDataPoint(dataPoint);
-		}
+    private class FakeDatastore implements Datastore {
+        private DataPointSet set;
+
+        private DataPointSet getSet() {
+            return set;
+        }
+
+        @Override
+        public void close() throws InterruptedException, DatastoreException {
+        }
+
+        @Override
+        public void putDataPoint(String metricName,
+                                 ImmutableSortedMap<String, String> tags,
+                                 DataPoint dataPoint, int ttl) throws DatastoreException {
+            if (set == null)
+                set = new DataPointSet(metricName, tags, Collections.EMPTY_LIST);
+
+            set.addDataPoint(dataPoint);
+        }
 
 		/*@Override
 		public void putDataPoints(DataPointSet dps) throws DatastoreException
@@ -379,38 +318,32 @@ public class PutCommandTest
 			this.set = dps;
 		}*/
 
-		@Override
-		public Iterable<String> getMetricNames() throws DatastoreException
-		{
-			return null;
-		}
+        @Override
+        public Iterable<String> getMetricNames() throws DatastoreException {
+            return null;
+        }
 
-		@Override
-		public Iterable<String> getTagNames() throws DatastoreException
-		{
-			return null;
-		}
+        @Override
+        public Iterable<String> getTagNames() throws DatastoreException {
+            return null;
+        }
 
-		@Override
-		public Iterable<String> getTagValues() throws DatastoreException
-		{
-			return null;
-		}
+        @Override
+        public Iterable<String> getTagValues() throws DatastoreException {
+            return null;
+        }
 
-		@Override
-		public void queryDatabase(DatastoreMetricQuery query, QueryCallback queryCallback) throws DatastoreException
-		{
-		}
+        @Override
+        public void queryDatabase(DatastoreMetricQuery query, QueryCallback queryCallback) throws DatastoreException {
+        }
 
-		@Override
-		public void deleteDataPoints(DatastoreMetricQuery deleteQuery) throws DatastoreException
-		{
-		}
+        @Override
+        public void deleteDataPoints(DatastoreMetricQuery deleteQuery) throws DatastoreException {
+        }
 
-		@Override
-		public TagSet queryMetricTags(DatastoreMetricQuery query) throws DatastoreException
-		{
-			return null;
-		}
-	}
+        @Override
+        public TagSet queryMetricTags(DatastoreMetricQuery query) throws DatastoreException {
+            return null;
+        }
+    }
 }
