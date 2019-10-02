e6f0c37ab00ff426cc24237b340cb1d3868c22a7
Alex Korotkykh <me@alexkorotkikh.com>
2019-10-02 11:36:33 +0200

    add logic and test

--- src/main/java/org/kairosdb/core/tiers/MetricTiersConfiguration.java
+++ src/main/java/org/kairosdb/core/tiers/MetricTiersConfiguration.java
@@ -1,96 +1,24 @@
 package org.kairosdb.core.tiers;
 
-import com.google.common.collect.ImmutableMap;
-import com.google.inject.Inject;
-import com.google.inject.name.Named;
-import org.apache.http.client.fluent.Executor;
-import org.apache.http.client.fluent.Request;
-import org.codehaus.jackson.JsonNode;
-import org.codehaus.jackson.map.ObjectMapper;
-import org.kairosdb.core.scheduler.KairosDBJob;
-import org.quartz.CronScheduleBuilder;
-import org.quartz.JobExecutionContext;
-import org.quartz.JobExecutionException;
-import org.quartz.Trigger;
-import org.zalando.stups.tokens.AccessToken;
-
-import java.io.IOException;
-import java.util.*;
-
-import static org.quartz.TriggerBuilder.newTrigger;
+import java.util.Map;
+import java.util.Set;
 
 public class MetricTiersConfiguration {
-    public class UpdateJob implements KairosDBJob {
-        private final ObjectMapper objectMapper;
-        private final Executor executor;
-        private final AccessToken accessToken;
-        private final String schedule;
-        private final String hostname;
 
-        @Inject
-        public UpdateJob(final ObjectMapper objectMapper,
-                         final Executor executor,
-                         final AccessToken accessToken,
-                         @Named("kairosdb.tiers.schedule") final String schedule,
-                         @Named("zmon.hostname") final String hostname) {
-            this.objectMapper = objectMapper;
-            this.executor = executor;
-            this.accessToken = accessToken;
-            this.schedule = schedule;
-            this.hostname = hostname;
-        }
+    private Set<Integer> criticalChecks;
+    private Set<Integer> importantChecks;
+    private Integer queryDistanceHoursLimit;
+    private Integer queryMaxCheckTier;
 
-        @Override
-        public Trigger getTrigger() {
-            return (newTrigger()
-                    .withIdentity(this.getClass().getSimpleName())
-                    .withSchedule(CronScheduleBuilder.cronSchedule(schedule))
-                    .build());
-        }
-
-        @Override
-        public void execute(final JobExecutionContext ctx) throws JobExecutionException {
-            try {
-                final Map<String, List<Integer>> checkTiers = getCheckTiers();
-                final Map<String, Integer> limitConfig = getLimitConfig();
-                update(checkTiers, limitConfig);
-            } catch (IOException e) {
-                // TODO
-            }
-        }
-
-        private Map<String, List<Integer>> getCheckTiers() throws IOException {
-            final String uri = hostname + "/api/v1/entities/zmon-check-tiers";
-            final Request request = Request.Get(uri).addHeader("Authorization", "Bearer " + accessToken.getToken());
-            final String response = executor.execute(request).returnContent().toString();
-            final JsonNode jsonNode = objectMapper.readTree(response);
-            final Optional<JsonNode> data = Optional.ofNullable(jsonNode.get("data"));
-
-            final List<Integer> criticalChecksList = getCheckList(data, "critical");
-            final List<Integer> importantChecksList = getCheckList(data, "important");
-
-            return ImmutableMap.of("critical", criticalChecksList, "important", importantChecksList);
-        }
-
-        private List<Integer> getCheckList(final Optional<JsonNode> data, final String kind) {
-            final Iterator<JsonNode> checks = data.map(d -> d.get(kind)).map(JsonNode::iterator).orElse(Collections.emptyIterator());
-            final List<Integer> checksList = new ArrayList<>();
-            while (checks.hasNext()) {
-                checksList.add(checks.next().getIntValue());
-            }
-            return checksList;
-        }
-
-        private Map<String, Integer> getLimitConfig() {
-            return null;
-        }
-    }
-
-    public int getQueryDistanceHoursLimit() {
-        return 0;
+    public Integer getQueryDistanceHoursLimit() {
+        return queryDistanceHoursLimit;
     }
 
     public boolean isMetricActiveForQuery(final String metricName) {
+        if (queryMaxCheckTier <= 0 || queryMaxCheckTier >= 3) {
+            return true;
+        }
+
         final String[] split = metricName.split("\\.");
         if (split.length != 3 || !split[0].equals("zmon") || !split[1].equals("check")) {
             return false;
@@ -102,11 +30,20 @@ public class MetricTiersConfiguration {
             return false;
         }
 
+        if (queryMaxCheckTier == 1) return criticalChecks.contains(checkId);
+        else if (queryMaxCheckTier == 2) return criticalChecks.contains(checkId) || importantChecks.contains(checkId);
+
         return false;
     }
 
-    private void update(final Map<String, List<Integer>> checkTiers,
+    void update(final Map<String, Set<Integer>> checkTiers,
                         final Map<String, Integer> limitConfig) {
+        this.criticalChecks = checkTiers.get("critical");
+        this.importantChecks = checkTiers.get("important");
+
+        this.queryDistanceHoursLimit = limitConfig.get("query_distance_hours_limit");
+        this.queryMaxCheckTier = limitConfig.get("query_max_check_tier");
+
     }
 
 }
