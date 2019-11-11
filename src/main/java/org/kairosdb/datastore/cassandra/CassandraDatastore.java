59daa262303d4a3f808554854bda865cb46beaa1
rajatparida86 <rajatparida86@gmail.com>
2019-11-11 16:38:19 +0100

    Introduce on the fly usage of `metric` as split index for wild card queries

--- src/main/java/org/kairosdb/datastore/cassandra/CassandraDatastore.java
+++ src/main/java/org/kairosdb/datastore/cassandra/CassandraDatastore.java
@@ -818,23 +818,26 @@ public class CassandraDatastore implements Datastore, KairosMetricReporter {
         Span span = tracer.buildSpan("query_index").start();
 
         try (Scope scope = tracer.scopeManager().activate(span, false)) {
-
             final List<String> indexTags = getIndexTags(query.getName());
             SetMultimap<String, String> filterTags = query.getTags();
-            for (String split : indexTags) {
-                if (filterTags.containsKey(split)) {
-                    Set<String> currentSet = filterTags.get(split);
-                    final boolean currentSetIsSmaller = currentSet.size() < useSplitSet.size();
-                    final boolean currentSetIsNotEmpty = currentSet.size() > 0 && useSplitSet.isEmpty();
-                    final boolean currentSetHasNoWildcards = currentSet.stream().noneMatch(x -> x.contains("*") || x.contains("?"));
-                    if ((currentSetIsSmaller || currentSetIsNotEmpty) && currentSetHasNoWildcards) {
-                        useSplitSet = currentSet;
-                        useSplitField = split;
+            if (useMetricAsSplitField(indexTags, filterTags)){
+                useSplitField = "metric";
+                useSplitSet = filterTags.get("key").stream().map((key) -> extractMetricsFromKey(key)).collect(Collectors.toSet());
+            } else {
+                for (String split : indexTags) {
+                    if (filterTags.containsKey(split)) {
+                        Set<String> currentSet = filterTags.get(split);
+                        final boolean currentSetIsSmaller = currentSet.size() < useSplitSet.size();
+                        final boolean currentSetIsNotEmpty = currentSet.size() > 0 && useSplitSet.isEmpty();
+                        final boolean currentSetHasNoWildcards = currentSet.stream().noneMatch(x -> x.contains("*") || x.contains("?"));
+                        if ((currentSetIsSmaller || currentSetIsNotEmpty) && currentSetHasNoWildcards) {
+                            useSplitSet = currentSet;
+                            useSplitField = split;
+                        }
                     }
                 }
             }
             List<String> useSplit = new ArrayList<>(useSplitSet);
-
             long startTime = calculateRowTimeRead(query.getStartTime());
             // Use write width here, as END time is upper bound for query and end with produces the bigger timestamp
             long endTime = calculateRowTimeWrite(query.getEndTime());
@@ -854,6 +857,39 @@ public class CassandraDatastore implements Datastore, KairosMetricReporter {
         } finally {
             span.finish();
         }
+    }
+
+    /* Rule when to use metric as split index key
+    - Only use metric if any value of key tag has wild cards
+    - If all other provided split index tags in the query do not have wildcards - Do not use metric as split index
+    - If metric does not have wildcards */
+    private boolean useMetricAsSplitField (List<String> indexTags, SetMultimap<String, String> filterTags) {
+        if (filterTags.containsKey("key")) {
+            Set<String> keyValues = filterTags.get("key");
+            boolean keyHasWildcards = keyValues.stream().anyMatch(x -> x.contains("*") || x.contains("?"));
+            if (keyHasWildcards && doAllSplitIndexTagsHaveWildCards(indexTags, filterTags)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private boolean doAllSplitIndexTagsHaveWildCards(List<String> indexTags, SetMultimap<String, String> filterTags) {
+        for (String splitTag: indexTags) {
+            if (filterTags.containsKey(splitTag)){
+                Set<String> tagValues = filterTags.get(splitTag);
+                if (tagValues.stream().noneMatch(x -> x.contains("*") || x.contains("?")) == true){
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    private String extractMetricsFromKey(String key) {
+        String[] segments = key.split(".");
+        String metric = segments[segments.length - 1];
+        return metric.contains("*") || metric.contains("?") ? "" : metric;
     }
 
     private List<DataPointsRowKey> getMatchingRowKeysFromRegularIndex(DatastoreMetricQuery query,
